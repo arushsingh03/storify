5ee0c6241f0d0dc4b306feed10435fd61b3788ab
Arush Singh <sarushmad@gmail.com>
2024-11-13 08:40:51 +0530

    added routes for sign-in/sign-up & configs for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Storify Let's Store, Share, and chill...😎</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Storify - Let's Store, Share, and chill...</h1>
     </div>
   );
 }

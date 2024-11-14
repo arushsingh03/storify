e9db07c3d6344676c7955f4723d7499d69f16087
Arush Singh <sarushmad@gmail.com>
2024-11-14 21:36:59 +0530

    added sign-in layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,20 +11,20 @@ const config: Config = {
     extend: {
       colors: {
         brand: {
-          DEFAULT: '#FA7275',
-          100: '#EA6365',
+          DEFAULT: '#56B8FF',
+          100: '#4FA0E6',
         },
-        red: '#FF7474',
+        red: '#333F4E',
         error: '#b80000',
         green: '#3DD9B3',
         blue: '#56B8FF',
         pink: '#EEA8FD',
         orange: '#F9AB72',
         light: {
-          100: '#333F4E',
-          200: '#A3B2C7',
-          300: '#F2F5F9',
-          400: '#F2F4F8',
+          100: '#A3B2C7',
+          200: '#F2F5F9',
+          300: '#F2F4F8',
+          400: '#FFFFFF',
         },
         dark: {
           100: '#04050C',

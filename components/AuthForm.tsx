a11705ff9452ae42472f334bc1608f0b2aa626dc
Arush Singh <sarushmad@gmail.com>
2024-11-19 22:17:32 +0530

    added mobile nav and sign-out functionality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,7 +17,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import OTPModal from "./OTPModal";
-import { createAccount } from "@/lib/actions/user.actions";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -51,10 +51,13 @@ const AuthForm = ({ type }: { type: FormType }) => {
     setErrorMessage("");
 
     try {
-      const user = await createAccount({
-        fullName: values.fullName || "",
-        email: values.email,
-      });
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
 
       setAccountId(user.accountId);
     } catch {
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Button } from "./ui/button";
-import Image from "next/image";
 import Search from "./Search";
+import Image from "next/image";
+import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 const Header = () => {
   return (
@@ -10,11 +11,23 @@ const Header = () => {
       <Search />
       <div className="header-wrapper">
         <FileUploader />
-        <form>
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
           {" "}
           <Button type="submit" className="sign-out-button">
-                <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24} className="w-6" />
-            </Button>{" "}
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>{" "}
         </form>
       </div>
     </header>
